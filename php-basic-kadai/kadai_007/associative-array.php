d279942f85824724a74f3407138d9d8c40963eb2
Yosuke-Morishita <144458024+Yosuke-Morishita@users.noreply.github.com>
2023-10-09 10:33:53 +0900

    kadai007追加

---
+++ php-basic-kadai/kadai_007/associative-array.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <body>
+        <p>
+            <?php
+            $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+            print_r($vegetables);
+            ?>
+            </p>
+</body>
+    </html>
